b8b41ad8215ec3d83e02114ae65f3c62e07583ca
zhangxin <zx13579003681@icloud.com>
2026-06-12 13:51:41 +0800

    add login page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -29,6 +29,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
